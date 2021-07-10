0d295b8c5abde859a52886503928df6d32b31485
Jeren Annagurbanova <67991797+Jeren-A@users.noreply.github.com>
2021-07-10 12:46:38 +0300

    update

---
+++ main.py
@@ -0,0 +1,27 @@
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+# SETTING PAGE CONFIG TO WIDE MODE
+st.set_page_config(layout="wide")
+
+# LOADING DATA
+DATE_TIME = "date/time"
+DATA_URL = (
+    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+)
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    return data
+
+data = load_data(100000)
+
+
+st.dataframe(data)
