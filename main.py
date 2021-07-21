208d190fc8034391c6b4680eb375f7739cc19d0e
Dogukan Kefeli <dogukankefeli@gmail.com>
2021-07-21 22:07:06 +0300

    latest working example

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def display_toots(username_input):
     id_searched = pure.get_user_id(username_input)
     user_toots_df = pure.get_user_toots(id_searched)
     st.markdown("User's toots")
-    st.dataframe(user_toots_df)
+    st.dataframe(user_toots_df[['toot_time','content','favourites_count']])
  
 # SETTING PAGE CONFIG TO WIDE MODE
 st.set_page_config(page_title = 'Dashboard', page_icon = '💯')
@@ -32,25 +32,10 @@ try:
     id_searched = pure.get_user_id(username_input)
     user_toots_df = pure.get_user_toots(id_searched)
     fig2 = px.scatter(user_toots_df, x="toot_time", y="favourites_count", hover_name="usernames", hover_data=["content", "user_ids"], )
-    fig2.update_layout(plot_bgcolor = '#0E1117')
+    fig2.update_layout(plot_bgcolor = '#CCCCCC')
     st.plotly_chart(fig2)
-except:
-    pass
+except Exception as ex:
 
-
-"""
-pure.get_timeline_users()
-df1 = pure.create_df()
-#st.table(df1)
-st.dataframe(df1)
-
-
-
-
-
-fig = px.scatter(df1, x="toot_time", y="favourites_count", hover_name="usernames", hover_data=["content", "user_ids"], )
-fig.update_layout(plot_bgcolor = '#0E1117')
-
-
-st.plotly_chart(fig)
-"""
+    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+    message = template.format(type(ex).__name__, ex.args)
+    print (message)
