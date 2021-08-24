203607a452ef4d97ba7ecb33d61a8aa263523c62
JEREN ANNAGURBANOVA <jerena@sabanciuniv.edu>
2021-08-24 11:15:50 +0300

    added access token

--- bot_scripts/mentionreply.py
+++ bot_scripts/mentionreply.py
@@ -1,13 +1,14 @@
 from mastobot import Bot
 bot = Bot(
-    instance_url="https://mastodon.instance",
-    access_token="NaaVC6ZOQY1Yf1Oz06XizwtPhynbTy16BuuNEpFAGPsn",
+    instance_url="https://mastodon.social",
+    access_token="NaaVC6ZOQY1Yf1Oz06XizwtPhynbTy16BuuNEpFAGPs",
 )
+
 
 @bot.on_mention("hi")
 def respond_to_hi(status):
-    name = status.account.username
+    name = status.account.display_name
     return f"hey, {name}!"
 
-
 bot.run()
+
